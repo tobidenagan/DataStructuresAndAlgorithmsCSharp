888d59f3e3c7095287ecf2830fa5af1c9776cd00
Tobi Denagan <denagantobi@gmail.com>
2024-08-01 15:20:39 +0100

    Add project files.

---
+++ LeetCodeQuestions.cs
@@ -0,0 +1,43 @@
+﻿namespace DataStructuresAndAlgorithmsCSharp
+{
+    public static class LeetCodeQuestions
+    {
+        public static int CountSeniors(string[] details)
+        {
+            int count = 0;
+            if (details.Length < 1) return count;
+            if (!(details.Length < 101)) return count;
+            foreach (string item in details)
+            {
+                string ageString = item.Substring(item.Length - 4, 2);
+                 _ = int.TryParse(ageString, out int age);
+                if(age > 60) count++;
+            }
+            return count;
+        }
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int numberOfLoops = nums.Length * nums.Length;
+            int[] result = new int[2];
+            int x = 0;
+            int y = 0;
+            for (int i = 0; i < numberOfLoops; i++)
+            {
+                if (i % nums.Length == 0) 
+                {
+                    x = 0;
+                    y++;
+                }
+                if (x == y ) continue;
+                if (nums[x] + nums[y] == target)
+                {
+                    result[0] = x;
+                    result[1] = y;
+                    return result;
+                }
+                x++;
+            }
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DataStructuresAndAlgorithmsCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(LeetCodeQuestions.CountSeniors(["7868190130M7522", "5303914400F9211", "9273338290F4010"]));
+            Console.WriteLine(LeetCodeQuestions.TwoSum([3, 2, 4], 6));
+            Console.WriteLine(LeetCodeQuestions.TwoSum([2, 7, 11, 15], 9));
+        }
+    }
+}
